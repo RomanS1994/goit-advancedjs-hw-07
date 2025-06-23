3e8c9c0342ad04112ad5c7c19cee39e05a8bdd9e
Roman Stryzhka <rstrizhko1994@gmail.com>
2025-06-23 09:15:56 +0200

    completed hw 7

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -32,8 +32,16 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary += 10000;
+  }
 }
 
+const employee = new Employee("Roman", "qwe", 30000);
+console.log(employee);
+
+const manager = new Manager("Roman", "qwe", 30000);
+console.log(manager);
 
 export {};
